ffabc9df2663abdb327c2ae00de2537e11bc12f5
jtelg <jtelgarecz@gmail.com>
2024-12-01 21:26:43 -0300

    change url production site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/icon.webp",
 
   // Set the production url of your site here
-  url: "https://cajarapida.com.ar",
+  url: "https://docs.cajarapida.com.ar",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
